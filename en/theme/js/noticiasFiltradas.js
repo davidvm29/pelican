167638368f6a249f205070741d994302907f703a
David Villen Moreno <dvillenmoreno@gmail.com>
2024-05-25 17:16:49 +0200

    Generate Pelican site

---
+++ en/theme/js/noticiasFiltradas.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Extraer y parsear los datos de sessionStorage
+  const newsData = JSON.parse(sessionStorage.getItem('newsForDate'));
+  
+  let htmlContent = '';
+  if (newsData && newsData.length) {
+    const newsContainer = document.getElementById('newsContainer') || document.createElement('div');
+    if (!document.getElementById('newsContainer')) {
+      newsContainer.setAttribute('id', 'newsContainer');
+      document.body.appendChild(newsContainer);
+    }
+
+    // Generar el encabezado basado en el idioma
+    if (idioma === "es") {
+      htmlContent += '<div class="elementosfiltrados"> Actualidad Universitaria - Elementos filtrados por fecha:  </div>';
+    } else {
+      htmlContent += '<div class="elementosfiltrados"> University News - Items filtered by date:   </div>';
+    }
+
+    // Procesar cada conjunto de 4 elementos como una noticia
+    for(let i = 0; i < newsData.length; i += 4) {
+      const link = newsData[i];
+      const titulo = newsData[i+1];
+      const fecha = newsData[i+2];
+      const lugar = newsData[i+3];
+
+      // Añadir el contenido HTML para cada noticia
+      htmlContent += '<div class="itemContainer">';
+      htmlContent += '<span>' + fecha + '</span>';
+      htmlContent += '<h2 class="titulofiltradas"><a href="' + link + '">' + titulo + '</a></h2>';
+      htmlContent += '<div class="genericItemCategory">';
+      if (idioma === "es") {
+        htmlContent += '<span>Publicado en</span>';
+      } else {
+        htmlContent += '<span>Published in</span>';
+      }
+      htmlContent += '<a id="lugar" href="#">' + lugar + '</a>';
+      if (idioma === "es") {
+        htmlContent += '<a href="' + link + '">&nbsp;Leer más...</a>';
+      } else {
+        htmlContent += '<a href="' + link + '">&nbsp;Read more...</a>';
+      }
+      htmlContent += '</div>';
+      htmlContent += '</div>';
+    }
+
+    // Establecer el contenido HTML del contenedor de noticias
+    newsContainer.innerHTML = htmlContent;
+  } else {
+    console.log("No hay datos de noticias disponibles");
+  }
+});
+
---
+++ es/theme/js/search.js
@@ -0,0 +1,72 @@
+let data;
+let idx;
+document.addEventListener('DOMContentLoaded', function() {
+    if (idioma === 'es'){
+        fetch('/theme/json/search_es.json')
+        .then(response => response.json())
+        .then(jsonData => {
+            data = jsonData;
+            idx = lunr(function () {
+                this.ref('title');
+                this.field('title');
+                this.field('url');
+    
+                data.forEach(doc => {
+                    doc.url = doc.url || '';
+                    this.add(doc);
+                });
+            });
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
+    } else {
+        fetch('/theme/json/search_en.json')
+        .then(response => response.json())
+        .then(jsonData => {
+            data = jsonData;
+            idx = lunr(function () {
+                this.ref('title');
+                this.field('title');
+                this.field('url');
+    
+                data.forEach(doc => {
+                    doc.url = doc.url || '';
+                    this.add(doc);
+                });
+            });
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error))
+    }
+
+    // Añade el listener al campo de texto para el evento input
+    document.getElementById('searchInput').addEventListener('input', performSearch);
+
+    // Añade el listener al botón para el evento click
+    document.getElementById('searchButton').addEventListener('click', performSearch);
+});
+
+function performSearch() {
+    var query = document.getElementById('searchInput').value.trim();
+    var resultsDiv = document.getElementById('searchResults');
+    resultsDiv.innerHTML = ''; // Limpia los resultados anteriores
+
+    if (query) { // Si hay una consulta, realiza la búsqueda
+        var results = idx.search(query);
+
+        // Muestra los resultados
+        results.forEach(result => {
+            var item = data.find(doc => doc.title === result.ref);
+
+            var li = document.createElement('li');
+            if (item && item.url) {
+                var link = document.createElement('a');
+                link.href = item.url;
+                link.textContent = item.title;
+                li.appendChild(link);
+            } else if (item) {
+                li.textContent = item.title;
+            }
+
+            resultsDiv.appendChild(li);
+        });
+    }
+}
