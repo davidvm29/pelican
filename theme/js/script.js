51e577403c9521b41bc16779700ed90762bb4b42
David Villen Moreno <dvillenmoreno@gmail.com>
2024-05-25 19:39:51 +0200

    Generate Pelican site

--- theme/js/script.js
+++ theme/js/script.js
@@ -163,7 +163,7 @@ function buscarYRedirigir(idioma) {
   var results = obtenerResultadosDeBusqueda(searchTerm);
   localStorage.setItem('searchResults', JSON.stringify(results));
   // Redireccionar a la página de búsqueda
-  window.location.href = idioma === "es" ? "/resultadosBusqueda.html" : "/en/resultadosBusqueda-en.html";
+  window.location.href = idioma === "es" ? SITEURL+"/resultadosBusqueda.html" : SITEURL+"/resultadosBusqueda-en.html";
 }
 
 function obtenerResultadosDeBusqueda(searchTerm) {
