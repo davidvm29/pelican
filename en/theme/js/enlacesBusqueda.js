409a2c0964fca18cb8de2036a1fe9758e8772b7a
David Villen Moreno <dvillenmoreno@gmail.com>
2024-06-09 22:23:40 +0200

    Generate Pelican site

--- en/theme/js/enlacesBusqueda.js
+++ en/theme/js/enlacesBusqueda.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 listItem.textContent = result.title;
                 // Enlazar el resultado a su URL correspondiente
                 var link = document.createElement('a');
-                    link.href = SITEURL + result.url;
+                    link.href = result.url;
                 link.appendChild(listItem);
                 resultsContainer.appendChild(link);
             });
